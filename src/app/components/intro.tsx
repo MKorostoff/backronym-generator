49ee2fe647701819f2716be01f2c71377dcbcbb2
Matt Korostoff <MKorostoff@gmail.com>
2023-05-05 17:43:29 -0400

    Note about offensive words

--- src/app/components/intro.tsx
+++ src/app/components/intro.tsx
@@ -9,9 +9,9 @@ export default function Intro() {
       <p className="intro-text">
       This is a hobby project, to learn all the new Nextjs APIs premiered this week at <a href="https://vercel.com/ship" target="_blank">vercel ship</a>. It was thrown together by Matt Korostoff in about 3 hours. It's meant to be the smallest possible app that would leverage <a href="https://vercel.com/docs/storage">vercel storage</a>, <a href="https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions">server actions</a>, <a href="https://nextjs.org/docs/getting-started/react-essentials#client-components">client/server components</a>, and <a href="https://nextjs.org/docs/app/api-reference/file-conventions/metadata">file based metadata</a>. It's not really meant to be <em>useful</em> just sort of a fun way to show off some new things you can do with Nextjs/Vercel.
       </p>
-
+      <h2>Warning</h2>
       <p className="intro-text">
-      Please don't share outside of FFW, it's just meant as a developer reference. <a href="https://github.com/MKorostoff/backronym-generator">Source code here</a>.
+      Please don't share outside of FFW, it's just meant as a developer reference. <a href="https://github.com/MKorostoff/backronym-generator">Source code here</a>. I tried to eliminate all the offensive words from the wordlist, but if I missed any, let me know on slack and I'll remove them (and do me a favor by not voting for them on the leaderboard if you find one).
       </p>
     </div>
   )
